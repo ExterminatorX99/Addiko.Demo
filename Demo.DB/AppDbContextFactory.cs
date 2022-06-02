b92a7a4668a87150ed0eff394fa899d661de0c1b
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-06-02 18:53:46 +0200

    Fix scaffolding

---
+++ Demo.DB/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Demo.DB
+{
+	public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		private const string ConnectionString = "Data Source=localhost;Initial Catalog=Demo;User ID=ap_demo;Password=ap_demo;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+
+		public AppDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+			optionsBuilder.UseSqlServer();
+
+			return new AppDbContext(optionsBuilder.Options);
+		}
+	}
+}
